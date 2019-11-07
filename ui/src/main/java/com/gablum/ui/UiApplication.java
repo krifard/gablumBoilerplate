f9a10eae442110121ced81b6097b510ddc042ac1
ananyo sen <ananyo.sen@boeing.com>
2019-11-07 21:59:50 +0530

    add gitignore

--- ui/src/main/java/com/gablum/ui/UiApplication.java
+++ ui/src/main/java/com/gablum/ui/UiApplication.java
@@ -2,8 +2,11 @@ package com.gablum.ui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UiApplication {
 
 	public static void main(String[] args) {
