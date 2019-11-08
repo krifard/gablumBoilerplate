42ccbedac3f7f1ad733d79abc11c618283bb1441
ananyo sen <ananyo.sen@boeing.com>
2019-11-08 17:46:09 +0530

    multi module service auction added

---
+++ auction/bid/src/test/java/com/gablum/auction/bid/BidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gablum.auction.bid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
