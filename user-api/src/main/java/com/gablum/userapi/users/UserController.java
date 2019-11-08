dab866c989985c03a20480bceacdde1801016191
ananyo sen <ananyo.sen@boeing.com>
2019-11-08 16:54:18 +0530

    dockerize

--- user-api/src/main/java/com/gablum/userapi/users/UserController.java
+++ user-api/src/main/java/com/gablum/userapi/users/UserController.java
@@ -1,4 +1,4 @@
-package com.gablum.userapi.user;
+package com.gablum.userapi.users;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
