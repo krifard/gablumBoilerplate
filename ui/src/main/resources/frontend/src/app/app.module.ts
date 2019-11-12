3584144189c91e6068f3febb9975c81d5f8e35a1
ananyo sen <ananyo.sen@boeing.com>
2019-11-12 10:23:03 +0530

    starting with angular
    created a navbar component and material module component

--- ui/src/main/resources/frontend/src/app/app.module.ts
+++ ui/src/main/resources/frontend/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AppMaterialModule } from './app-material/app-material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AppMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
